ba2550487e950674a461752268ee94e4695c33b9
Sandra_SFR <sandrafantonirubio@gmail.com>
2021-12-22 19:49:50 +0100

    semilla del proyecto

---
+++ src/main/java/com/svalero/aasandramultihilo/AppController.java
@@ -0,0 +1,58 @@
+package com.svalero.aasandramultihilo;
+
+import com.svalero.aasandramultihilo.util.R;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppController {
+    public TextField tfUrl;
+    public TabPane tpDownloads;
+    public String directorio="descargas";
+
+    private Map<String, DownloadController> allDownloads;
+
+    public AppController() {
+        allDownloads = new HashMap<>();
+    }
+
+    @FXML
+    public void launchDownload(ActionEvent event) {
+        String urlText = tfUrl.getText();
+        tfUrl.clear();
+        tfUrl.requestFocus();
+
+        launchDownload(urlText);
+    }
+
+    private void launchDownload(String url) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(R.getUI("download.fxml"));
+
+            DownloadController downloadController = new DownloadController(url);
+            loader.setController(downloadController);
+            VBox downloadBox = loader.load();
+
+            String filename = url.substring(url.lastIndexOf("/") + 1); //nombre que aparece en la pestaña
+            tpDownloads.getTabs().add(new Tab(filename, downloadBox)); //se genera en la sección del panel tpDownloads
+
+            allDownloads.put(url, downloadController);
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void stopAllDownloads() {
+        for (DownloadController downloadController : allDownloads.values())
+            downloadController.stop();
+    }
+}
